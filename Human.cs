d938eff830b314faf92a08c58d64c6df226c6e5c
TomsLielkalns <tomslielkalns@gmail.com>
2020-06-21 16:20:54 +0300

    Added banking class

--- Human.cs
+++ Human.cs
@@ -1,24 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace ConsoleAppExperiments
 {
     class Human
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public int Age { get; set; }
+        public string Name { get; }
+        public string Surname { get; }
+        public int Age { get; private set; }
         public string EyeColor { get; set; }
 
-        public Human()
+        public Human(string name, string surname, int age, string eyeColor)
         {
-
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            this.EyeColor = eyeColor;
         }
 
         public void Introduce()
         {
-            Console.WriteLine("Hi, my full name is " + this.Name + " " + this.Surname + ". " + "I am " + this.Age + " years old");
+            Console.WriteLine($"Hi, my full name is {this.Name} {this.Surname}. I am {this.Age} years old.");
+        }
+
+        public void AddNumbers(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public void SayName()
+        {
+            Console.WriteLine(this.Name);
+        }
+
+        public void SetAge(int age)
+        {
+            if(age < 0)
+            {
+                return;
+            }
+            else if (age < this.Age)
+            {
+                Console.WriteLine("The new age for " + this.Name + " is younger, are you sure you want to change? y/n");
+                string answer = Console.ReadLine();
+                if(answer == "y")
+                {
+                    this.Age = age;
+                }
+                else
+                {
+                    return;
+                }
+
+            }
+            else
+            {
+                this.Age = age;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleAppExperiments
 {
@@ -6,15 +7,27 @@ namespace ConsoleAppExperiments
     {
         static void Main(string[] args)
         {
-            Human person = new Human();
-            person.Name = "Jenifer";
-            person.Surname = "Lopez";
-            person.Age = 25;
-            person.EyeColor = "Brown";
-            person.Introduce();
+            List<Human> persons = new List<Human>();
+            persons.Add(new Human("Jenifer", "Lopez", 25, "Brown"));
+            persons.Add(new Human("Anna", "Smith", 15, "White"));
+            persons.Add(new Human("Mikelis", "Zalkajis", 17, "Green"));
+            persons.Add(new Human("Agnese", "Williams", 19, "Red"));
 
-            person.Name = "Anna";
-            person.Introduce();
+            //persons[1].SetAge(14);
+
+            foreach(var person in persons)
+            {
+                person.Introduce();
+            }
+
+            //create some accounts
+            List<BankAccount> accounts = new List<BankAccount>();
+            accounts.Add(new BankAccount("Jenifer", 200, "EUR"));
+            accounts[0].PrintInfo();
+            accounts.Add(new BankAccount("Anna", 5000, "EUR"));
+            accounts[1].PrintInfo();
+            accounts.Add(new BankAccount("Michael", 450, "EUR"));
+            accounts[2].PrintInfo();
         }
     }
 }
